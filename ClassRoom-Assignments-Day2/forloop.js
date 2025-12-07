78eb7fe80682ee0a16468dc45153e114b4d6bedd
someswarang <someswaran.gambhiram@gmail.com>
2025-12-07 11:10:40 +0530

    Add files via upload
    Committing Week 1 - Day 2 Class Room Assignments

---
+++ ClassRoom-Assignments-Day2/forloop.js
@@ -0,0 +1,9 @@
+//To Print the Odd Number from 0 to 30 using for loop
+
+for(let num=0;num<=30;num++){
+
+    if (num%2!=0){
+        console.log ("Number is :"+num+" a odd number")
+    }
+    
+}
---
+++ ClassRoom-Assignments-Day2/ifelse.js
@@ -0,0 +1,18 @@
+//To print the Grade System using if else
+
+// Declared variable as mark and assigned a value to it
+let mark = 55
+
+//Using if else condition writing logic to print it's grade
+if (mark>85){
+    console.log("Grade is : A")
+}
+else if(mark>60){
+    console.log("Grade is : B")
+}
+else if(mark>50){
+    console.log("Grade is : C")
+}
+else{
+    console.log("Grade is :D")
+}
